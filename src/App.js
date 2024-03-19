857f188e7a7dc18d9049ab12eb42493b2e855336
Hannah Z <eveline1129@163.com>
2024-03-19 18:07:31 -0500

    started migrating to react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import "./style.css"
+
 function App() {
-	return <h1>test</h1>
+	return (
+		<header className='header'>
+			<div className='logo'>
+				<img
+					src='logo.png'
+					height='68'
+					width='68'
+					alt='Today I learned Logo'
+				/>
+				<h1>Today I Learned</h1>
+			</div>
+
+			<button class='btn btn-large btn-open'>Share a fact</button>
+		</header>
+	)
 }
 
 export default App
