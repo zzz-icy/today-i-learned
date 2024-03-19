903b95816e8dffdd63352e12865dc92764aba75f
Hannah Z <eveline1129@163.com>
2024-03-19 17:47:41 -0500

    added simple script

---
+++ script.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector(".btn-open")
+const form = document.querySelector(".fact-form")
+
+btn.addEventListener("click", () => {
+	if (form.classList.contains("hidden")) {
+		form.classList.remove("hidden")
+		btn.textContent = "Close"
+	} else {
+		form.classList.add("hidden")
+		btn.textContent = "share a fact"
+	}
+})
