630f3606850399487cd478bba0abb5fc893c3ac7
Hannah Z <eveline1129@163.com>
2024-03-19 20:35:37 -0500

    header comp and form comp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import "./style.css"
 import { CATEGORIES, initialFacts } from "./data"
+import { useState } from "react"
 
 function App() {
-	const appTitle = "Today I Learned"
+	const [showForm, setShowForm] = useState(false)
 
 	return (
 		<>
 			{/* HEADER */}
-			<header className='header'>
-				<div className='logo'>
-					<img
-						src='logo.png'
-						height='68'
-						width='68'
-						alt='Today I learned Logo'
-					/>
-					<h1>{appTitle}</h1>
-				</div>
-
-				<button className='btn btn-large btn-open'>Share a fact</button>
-			</header>
-			<NewFactForm />
+			<Header
+				setShowForm={setShowForm}
+				showForm={showForm}
+			/>
+			{showForm ? <NewFactForm /> : null}
 			<main className='main'>
 				<CategoryFilter />
 				<FactList />
@@ -28,11 +20,108 @@ function App() {
 		</>
 	)
 }
+const Header = ({ setShowForm, showForm }) => {
+	const appTitle = "Today I Learned"
+
+	return (
+		<header className='header'>
+			<div className='logo'>
+				<img
+					src='logo.png'
+					height='68'
+					width='68'
+					alt='Today I learned Logo'
+				/>
+				<h1>{appTitle}</h1>
+			</div>
+
+			<button
+				className='btn btn-large btn-open'
+				onClick={() => setShowForm((showForm) => !showForm)}
+			>
+				{showForm ? "close" : "Share a fact"}
+			</button>
+		</header>
+	)
+}
 const NewFactForm = () => {
-	return <form className='fact-form'>Fact Form</form>
+	const [text, setText] = useState("")
+	const [source, setSource] = useState("")
+	const [category, setCategory] = useState("")
+	const handleSubmit = (e) => {
+		e.preventDefault()
+	}
+	return (
+		<form
+			className='fact-form'
+			onSubmit={handleSubmit}
+		>
+			<input
+				type='text'
+				value={text}
+				onChange={(e) => setText(e.target.value)}
+				placeholder='Share a fact with the world...'
+			/>
+			<span>{200 - text.length}</span>
+			<input
+				type='text'
+				value={source}
+				onChange={(e) => setSource(e.target.value)}
+				placeholder='Trustworthy source...'
+			/>
+			<select
+				value={category}
+				onChange={(e) => setCategory(e.target.value)}
+			>
+				<option value=''>Choose Category:</option>
+				{CATEGORIES.map((cat) => {
+					return (
+						<option
+							key={cat.name}
+							value={cat.name}
+						>
+							{cat.name.toUpperCase()}
+						</option>
+					)
+				})}
+			</select>
+			<button className='btn btn-large'>Post</button>
+		</form>
+	)
 }
 const CategoryFilter = () => {
-	return <aside>Category Filter</aside>
+	return (
+		<aside>
+			<ul>
+				{CATEGORIES.map((item, index) => {
+					if (index === 0) {
+						return (
+							<li
+								className='category'
+								key='all'
+							>
+								<button className='btn btn-all-categories'>All</button>
+							</li>
+						)
+					} else {
+						return (
+							<li
+								className='category'
+								key={item.name}
+							>
+								<button
+									className='btn btn-category'
+									style={{ backgroundColor: item.color }}
+								>
+									{item.name}
+								</button>
+							</li>
+						)
+					}
+				})}
+			</ul>
+		</aside>
+	)
 }
 
 const FactList = () => {
@@ -42,44 +131,10 @@ const FactList = () => {
 			<ul className='facts-list'>
 				{facts.map((fact, index) => {
 					return (
-						<li
-							className='fact'
+						<Fact
+							data={fact}
 							key={fact.id}
-						>
-							<p>
-								{fact.text}
-								<a
-									className='source'
-									href={fact.source}
-									target='_blank'
-									rel='noreferrer'
-								>
-									(Source)
-								</a>
-							</p>
-							<span
-								className='tag'
-								style={{
-									backgroundColor: CATEGORIES.find(
-										(category) => category.name === fact.category
-									).color,
-								}}
-							>
-								{fact.category}
-							</span>
-
-							<div className='vote-buttons'>
-								<button>
-									👍 <strong>{fact.votesInteresting}</strong>
-								</button>
-								<button>
-									🤯 <strong>{fact.votesMindblowing}</strong>
-								</button>
-								<button>
-									⛔️ <strong>{fact.votesFalse}</strong>
-								</button>
-							</div>
-						</li>
+						/>
 					)
 				})}
 			</ul>
@@ -87,4 +142,43 @@ const FactList = () => {
 	)
 }
 
+const Fact = ({ data }) => {
+	return (
+		<li className='fact'>
+			<p>
+				{data.text}
+				<a
+					className='source'
+					href={data.source}
+					target='_blank'
+					rel='noreferrer'
+				>
+					(Source)
+				</a>
+			</p>
+			<span
+				className='tag'
+				style={{
+					backgroundColor: CATEGORIES.find(
+						(category) => category.name === data.category
+					).color,
+				}}
+			>
+				{data.category}
+			</span>
+
+			<div className='vote-buttons'>
+				<button>
+					👍 <strong>{data.votesInteresting}</strong>
+				</button>
+				<button>
+					🤯 <strong>{data.votesMindblowing}</strong>
+				</button>
+				<button>
+					⛔️ <strong>{data.votesFalse}</strong>
+				</button>
+			</div>
+		</li>
+	)
+}
 export default App
