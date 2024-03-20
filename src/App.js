9e5edba76abd1ac04477b7f0f7cb557c06b299fc
Hannah Z <eveline1129@163.com>
2024-03-19 19:57:37 -0500

    converted the fact list component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,89 @@
 import "./style.css"
+import { CATEGORIES, initialFacts } from "./data"
 
 function App() {
-	return (
-		<header className='header'>
-			<div className='logo'>
-				<img
-					src='logo.png'
-					height='68'
-					width='68'
-					alt='Today I learned Logo'
-				/>
-				<h1>Today I Learned</h1>
-			</div>
+	const appTitle = "Today I Learned"
 
-			<button class='btn btn-large btn-open'>Share a fact</button>
-		</header>
+	return (
+		<>
+			{/* HEADER */}
+			<header className='header'>
+				<div className='logo'>
+					<img
+						src='logo.png'
+						height='68'
+						width='68'
+						alt='Today I learned Logo'
+					/>
+					<h1>{appTitle}</h1>
+				</div>
+
+				<button className='btn btn-large btn-open'>Share a fact</button>
+			</header>
+			<NewFactForm />
+			<main className='main'>
+				<CategoryFilter />
+				<FactList />
+			</main>
+		</>
+	)
+}
+const NewFactForm = () => {
+	return <form className='fact-form'>Fact Form</form>
+}
+const CategoryFilter = () => {
+	return <aside>Category Filter</aside>
+}
+
+const FactList = () => {
+	const facts = initialFacts
+	return (
+		<section>
+			<ul className='facts-list'>
+				{facts.map((fact, index) => {
+					return (
+						<li
+							className='fact'
+							key={fact.id}
+						>
+							<p>
+								{fact.text}
+								<a
+									className='source'
+									href={fact.source}
+									target='_blank'
+									rel='noreferrer'
+								>
+									(Source)
+								</a>
+							</p>
+							<span
+								className='tag'
+								style={{
+									backgroundColor: CATEGORIES.find(
+										(category) => category.name === fact.category
+									).color,
+								}}
+							>
+								{fact.category}
+							</span>
+
+							<div className='vote-buttons'>
+								<button>
+									👍 <strong>{fact.votesInteresting}</strong>
+								</button>
+								<button>
+									🤯 <strong>{fact.votesMindblowing}</strong>
+								</button>
+								<button>
+									⛔️ <strong>{fact.votesFalse}</strong>
+								</button>
+							</div>
+						</li>
+					)
+				})}
+			</ul>
+		</section>
 	)
 }
 
